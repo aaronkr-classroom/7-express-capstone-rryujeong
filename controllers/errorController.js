095c1b7c91f11e52ccc45519db68f924d5decf48
rryujeong <126834727+rryujeong@users.noreply.github.com>
2024-04-17 13:31:49 +0000

    hi-04-17-02

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,12 +1,16 @@
 // errorController.js
 "use strict";
+const httpStatus = require("http-status-codes");
 
 
 
 /**
  * Listing 11.2 (p. 168)
  */
-
+exports.logErrors = (err, req, res ,next) => {
+    console.error(err.stack);
+    next(err);
+}; // @TODO: 에러 처리를 위한 미들웨어 추가
 
 /**
  * Listing 12.11 (p. 184)
@@ -14,6 +18,18 @@
  */
 
 
+exports.pageNotFoundError = (req,res) => {
+    let errorCode = httpStatus.NOT_FOUND;
+    res.status(errorCode);
+    res.render("error");
+};
+
+exports.internalServerError = (error,req,res,next) => {
+    let errorCode = httpStatus.INTERNAL_SERVER_ERROR;
+    console.log(`ERROR occurred: ${error.stack}`);
+    res.status(errorCode);
+    res.send(`${errorCode} | Sorry, our application is taking a nap!`);
+};
 /**
  * [노트] 라우트 순서는 중요하다. 이 라우트는 기존에 존재하는 라우트 아래에 와야 한다.
  * 기존 라우트는 범용으로 사용되는 것이며 하위 라우트에 오버라이딩되기 때문이다.
--- main.js
+++ main.js
@@ -3,6 +3,17 @@
 "use strict";
 
 // 앱 설정
+// Best 습관 : 먼저모든 상수나 변수 정의 (const)
+const port = 3000,
+    express = require('express'),
+    layouts = require('express-ejs-layouts'),
+
+    homeController = require('./controllers/homeController'),
+    errorController = require('./controllers/errorController'),
+    app = express();
+
+    app.set("port", process.env.PORT || port);
+    app.set("view engine", "ejs");
 
 /**
  * Listing 12.7 (p. 179)
@@ -10,22 +21,39 @@
  */
 
 
+app.use(layouts);
+app.use(express.static("public"))
+
 /**
  * Listing 12.4 (p. 177)
  * body-parser의 추가
  */
+app.use(
+    express.urlencoded({
+        extended: false
+    })
+);
+app.use(express.json());
 
 
 /**
  * Listing 12.6 (p. 178)
  * 각 페이지 및 요청 타입을 위한 라우트 추가
  */
-
+app.get("/",homeController.showIndex);
+app.get("/courses", homeController.showCourses);
+app.get("/contact",homeController.showSignUp);
+app.post("/contact", homeController.postedContactForm);
 
 /**
  * Listing 12.12 (p. 184)
  * 에러 처리 라우트 
  */
-
+app.use(errorController.pageNotFoundError);
+app.use(errorController.internalServerError);
+app.use(errorController.internalServerError);
 
 // 3000번 포트로 리스닝 설정
+app.listen(app.get("port"), () => {
+    console.log(`Server at: http://localhost:${app.get("port")}`);
+});
